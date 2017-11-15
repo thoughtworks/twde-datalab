0cb2107f8fc0c72011494dbe55dc8d92150ac0c6
Emily A Grasmeder <egrasmed@thoughtworks.com>
2017-11-15 13:09:54 +0100

    refine test to actually test scoring algorithm, ignore .cache dir

--- src/evaluation.py
+++ src/evaluation.py
@@ -1,2 +1,6 @@
-def nwrmsle(estimate, actual):
-    return 9919
+import numpy as np
+
+
+def nwrmsle(predictions, targets, weights):
+    log_square_errors = (np.log(predictions + 1) - np.log(targets + 1)) ** 2
+    return(np.sqrt(np.sum(weights * log_square_errors) / np.sum(weights)))
