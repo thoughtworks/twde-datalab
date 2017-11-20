4fc622152a0e319f8662b971d983aae6def5b2e4
Arif Wider <awider@thoughtworks.com>
2017-11-20 10:57:00 +0100

    added zeppelin config change to python 3 in bootstrap action

--- deployment/bootstrap-action.sh
+++ deployment/bootstrap-action.sh
@@ -4,7 +4,6 @@ set -o errexit
 set -o pipefail
 set -o nounset
 set -o xtrace
-  
 
 pip install --upgrade pip
 # Non-standard and non-Amazon Machine Image Python modules:
@@ -15,3 +14,9 @@ sudo pip install -U sklearn
 sudo pip install -U seaborn
 sudo pip install -U pandas
 sudo pip install -U boto
+
+sudo sed -i -e 's/"zeppelin.python": "python",/"zeppelin.python": "python3",/g' /etc/zeppelin/conf/interpreter.json
+sudo sed -i -e 's/"zeppelin.pyspark.python": "python",/"zeppelin.pyspark.python": "python3",/g' /etc/zeppelin/conf/interpreter.json
+sudo sed -i -e 's/"perNoteProcess": false,/"perNoteProcess": true,/g' /etc/zeppelin/conf/interpreter.json
+
+sudo /usr/lib/zeppelin/bin/zeppelin-daemon.sh stop
