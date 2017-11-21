9fcdd61ddb9c7f27a2340dd8c25d5b15cf973b07
Arif Wider <awider@thoughtworks.com>
2017-11-21 17:06:13 +0100

    adding boto3 python package

--- deployment/bootstrap-action.sh
+++ deployment/bootstrap-action.sh
@@ -13,4 +13,5 @@ sudo python3 -m pip install awscli
 sudo python3 -m pip install plotly
 sudo python3 -m pip install sklearn
 sudo python3 -m pip install seaborn
+sudo python3 -m pip install boto3
 
