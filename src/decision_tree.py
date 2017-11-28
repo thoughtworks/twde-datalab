4fce78ca7f6211006af43ed587df1a76adeaacc5
Emily A Grasmeder <egrasmed@thoughtworks.com>
2017-11-28 12:08:06 +0100

    Automate decision tree, output meaningful data

--- src/decision_tree.py
+++ src/decision_tree.py
@@ -4,6 +4,8 @@ import sys
 import os
 import s3fs
 import boto3
+import s3io
+import joblib
 from io import StringIO
 sys.path.append(os.path.join('..', 'src'))
 sys.path.append(os.path.join('src'))
@@ -13,8 +15,8 @@ import evaluation
 
 
 def load_data(s3, s3bucket):
-
-    latestContents = s3.get_object(Bucket='twde-datalab', Key='splitter/latest')['Body']
+    dataset = "latest"
+    latestContents = s3.get_object(Bucket='twde-datalab', Key='splitter/{}'.format(dataset))['Body']
     latest = latestContents.read().decode('utf-8').strip()
 
     trainPath = "splitter/{}/train.csv".format(latest)
@@ -79,34 +81,53 @@ def make_predictions(clf, validate):
     return validate_preds
 
 
-def write_predictions_and_score_to_s3(s3, s3bucket, test_predictions, validation_score, model):
+def write_predictions_and_score_to_s3(s3, s3bucket, test_predictions, validation_score, model, timestamp, test, columns_used):
     aws_akid = os.environ['AWS_ID']
     aws_seckey = os.environ['AWS_SECRET']
 
-    timestamp = datetime.datetime.now().isoformat()
-    s3path = "s3://{s3bucket}/decision_tree/{timestamp}/".format(s3bucket=s3bucket, timestamp=timestamp)
+    key = "decision_tree/{timestamp}".format(timestamp=timestamp.isoformat())
+    s3path = "s3://{s3bucket}/{key}/".format(s3bucket=s3bucket, key=key)
 
     fs = s3fs.S3FileSystem(key=aws_akid, secret=aws_seckey)
 
-    s3.put_object(Body=timestamp, Bucket=s3bucket, Key='decision_tree/latest')
+    s3.put_object(Body=timestamp.isoformat(), Bucket=s3bucket, Key='decision_tree/latest')
 
     print("Writing test_predictions to {}".format(s3path))
-    bytes_to_write = pd.DataFrame({'unit_sales': test_predictions}).to_csv(None, index=False).encode()
+    test_predictions = pd.DataFrame({'unit_sales': test_predictions})
+    predictions = test.join(test_predictions).drop(['date', 'store_nbr', 'item_nbr', 'onpromotion'], axis=1)
+    bytes_to_write = predictions.to_csv(None, index=False).encode()
     with fs.open(s3path + 'test_predictions.csv', 'wb') as f:
        f.write(bytes_to_write)
 
-    print("Writing validation_score to {}".format(s3path))
-    bytes_to_write = pd.DataFrame({'estimate': [validation_score]}).to_csv(None, index=False).encode()
+    print("Writing model as pickle to {}".format(s3path))
+    compress = ('gzip', 3)
+    credentials = dict(
+        aws_access_key_id=aws_akid,
+        aws_secret_access_key=aws_seckey,
+    )
+    with s3io.open('s3://{0}/{1}'.format(s3bucket, key), mode='w', **credentials) as s3_file:
+        joblib.dump(model, s3_file, compress=compress)
+
+    print("Writing validation_score and metadata to {}".format(s3path))
+    timediff = (datetime.datetime.now() - timestamp).total_seconds() / 60
+    bytes_to_write = pd.DataFrame({'runtime_minutes': [timediff], 'estimate': [validation_score], 'columns_used': [columns_used]}).to_csv(None, index=False).encode()
     with fs.open(s3path + 'validation_score.csv', 'wb') as f:
         f.write(bytes_to_write)
+    print("Done. Time elapsed (minutes): {timediff}".format(timediff=timediff))
+
+    # To open this pickle:
+    # with s3io.open('s3://{0}/{1}'.format(bucket, key), mode='r', **credentials) as s3_file:
+    #     obj_reloaded = joblib.load(s3_file)
 
 
 if __name__ == "__main__":
+
+    timestamp = datetime.datetime.now()
     s3bucket = "twde-datalab"
     s3 = boto3.client('s3')
 
-    train, validate, test = load_data(s3, s3bucket)
-    train, validate, test = encode(train, validate, test)
+    original_train, original_validate, original_test = load_data(s3, s3bucket)
+    train, validate, test = encode(original_train, original_validate, original_test)
     model = make_model(train)
     validation_predictions = make_predictions(model, validate)
 
@@ -115,4 +136,4 @@ if __name__ == "__main__":
 
     test_predictions = make_predictions(model, test)
 
-    write_predictions_and_score_to_s3(s3, s3bucket, test_predictions, validation_score, model)
+    write_predictions_and_score_to_s3(s3, s3bucket, test_predictions, validation_score, model, timestamp, original_test, original_train.columns)
