ad9132154c8553fd90fc2a5021298702727c92bc
Arif Wider <awider@thoughtworks.com>
2017-12-19 16:08:49 +0100

    fixed time series to get score, added prophet installation into shell script (requires conda)

--- run_time_series_pipeline.sh
+++ run_time_series_pipeline.sh
@@ -2,6 +2,9 @@
 
 set -e
 
+conda install gcc -y
+conda install -c conda-forge fbprophet -y
+
 python3 src/merger.py
 python3 src/splitter.py
 python3 src/prophet_time_series.py
--- src/evaluation.py
+++ src/evaluation.py
@@ -1,13 +1,12 @@
 import numpy as np
 import pandas as pd
 
-
 def nwrmsle(predictions, targets, weights):
     if type(predictions) == list:
         predictions = np.array([np.nan if x < 0 else x for x in predictions])
     elif type(predictions) == pd.Series:
-        predictions[predictions < 0] = np.nan
-    targets[targets < 0] = np.nan
+        predictions.loc[predictions < 0] = np.nan
+    targets.loc[targets < 0] = np.nan
     weights = 1 + 0.25 * weights
     log_square_errors = (np.log(predictions + 1) - np.log(targets + 1)) ** 2
     return(np.sqrt(np.sum(weights * log_square_errors) / np.sum(weights)))
