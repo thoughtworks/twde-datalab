599b7e73c0c86f065dbbb2946573aa3ba83419f3
Arif Wider <awider@thoughtworks.com>
2017-12-20 16:15:06 +0100

    fixed tests

--- test/splitter_test.py
+++ test/splitter_test.py
@@ -3,11 +3,11 @@ import os
 import pandas as pd
 sys.path.append(os.path.join('..', 'src'))
 sys.path.append(os.path.join('src'))
-import load_and_split
+import splitter
 
 def test_get_validation_period():
     latest_date = pd.to_datetime('2017-11-22')
-    actual_begin_date, actual_end_date = load_and_split.get_validation_period(latest_date)
+    actual_begin_date, actual_end_date = splitter.get_validation_period(latest_date)
     expected_begin_date = pd.to_datetime('2017-11-01')
     expected_end_date = pd.to_datetime('2017-11-16')
     assert actual_begin_date == expected_begin_date
@@ -22,6 +22,6 @@ def test_split_validation_train_by_validation_period():
     validation_end_date = pd.to_datetime('2017-11-30')
     d = {'date': [date1, date2, date3, date4], 'col2': [3, 4, 5, 6]}
     df = pd.DataFrame(data=d)
-    df_train, df_validation = load_and_split.split_validation_train_by_validation_period(df, validation_begin_date, validation_end_date)
+    df_train, df_validation = splitter.split_validation_train_by_validation_period(df, validation_begin_date, validation_end_date)
     assert df_train.shape[0] == 1
     assert df_validation.shape[0] == 2
